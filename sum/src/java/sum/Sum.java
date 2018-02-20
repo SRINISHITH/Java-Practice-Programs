dd0284df65ae3194dc2584eccf65fa5475b94e43
SRINISHITH <srinishith64@gmail.com>
2018-02-20 12:23:17 +0530

    This is my first commit

---
+++ sum/src/java/sum/Sum.java
@@ -0,0 +1,17 @@
+package java.sum;
+
+public class Sum 
+{
+	static int getsum (int x, int y)
+	{
+		return x+y;
+	}
+ public static void main(String[] args)
+{
+	getsum (10,20);
+	System.out.println(getsum(10,20));
+}
+
+}
+
+
